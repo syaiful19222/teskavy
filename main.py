114def3716ce516ea271c0cbb395dfe228f92e4f
syaiful19222 <65751654+syaiful19222@users.noreply.github.com>
2023-10-13 17:01:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
-from kivy.lang import Builder
 from kivymd.app import MDApp
+from kivy.lang import Builder
 
-import kivymd_extensions.akivymd
-from kivymd.uix.boxlayout import MDBoxLayout
-from kivymd.uix.list import IRightBodyTouch
-class Container(IRightBodyTouch, MDBoxLayout):
-    adaptive_width = True
-
-class App(MDApp):
+class SampleApp(MDApp):
+    
     def build(self):
-        self.title = 'Vibot Pro Edition'
-        self.theme_cls.primary_palette = "Pink"
-        self.theme_cls.theme_style = "Dark"  # "Light"
-        return Builder.load_file('main.kv')
+        self.appKv='''
+MDScreen:
+    MDLabel:
+        text:'Hello,World.'
+        multiline:True
+        halign:'center'         
+'''
+        AppScreen=Builder.load_string(self.appKv)
+        return AppScreen
 
-
-App().run()
+SampleApp().run()
